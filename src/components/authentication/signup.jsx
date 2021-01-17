3190d17c330728c30cd9f5f1982e70c7b7b49a74
Ben McMann <bbmcmann@volosys.com>
2021-01-17 16:49:55 -0700

    Container and Row used to format input fields

--- src/components/authentication/signup.jsx
+++ src/components/authentication/signup.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 export default function SignUp() {
   return (
     <div className="m-3">
       Sign Up Page
+      <h1>Create an account</h1>
+      <Container>
+        <form>
+          <Row>
+            <input type="text" />
+          </Row>
+          <Row>
+            <input type="text" />
+            <input type="text" />
+          </Row>
+          <Row>
+            <input type="text" />
+            <input type="text" />
+          </Row>
+        </form>
+      </Container>
     </div>
   );
 }
