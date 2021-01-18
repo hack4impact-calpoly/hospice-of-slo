2410bdb83399488d8826e99b58162b24b14a1e35
Ben McMann <bbmcmann@volosys.com>
2021-01-17 18:00:40 -0700

    Skeleton layout done

--- src/components/authentication/signup.jsx
+++ src/components/authentication/signup.jsx
@@ -1,25 +1,57 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function SignUp() {
   return (
     <div className="m-3">
-      Sign Up Page
       <h1>Create an account</h1>
       <Container>
         <form>
           <Row>
-            <input type="text" />
+            <Col sm>
+              <p>Name</p>
+            </Col>
           </Row>
           <Row>
-            <input type="text" />
-            <input type="text" />
+            <Col sm>
+              <input type="text" />
+            </Col>
           </Row>
           <Row>
-            <input type="text" />
-            <input type="text" />
+            <Col sm>
+              <p>Email</p>
+            </Col>
+            <Col sm>
+              <p>Phone Number</p>
+            </Col>
           </Row>
+          <Row>
+            <Col sm>
+              <input type="text" />
+            </Col>
+            <Col sm>
+              <input type="text" />
+            </Col>
+          </Row>
+          <Row>
+            <Col sm>
+              <p>Password</p>
+            </Col>
+            <Col sm>
+              <p>Re-enter Password</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm>
+              <input type="text" />
+            </Col>
+            <Col sm>
+              <input type="text" />
+            </Col>
+          </Row>
+          <input type="button" />
         </form>
       </Container>
     </div>
