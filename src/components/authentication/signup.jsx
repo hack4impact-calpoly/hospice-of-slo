0a212529785dab8b419899433debbce6378888dc
Ben McMann <bbmcmann@volosys.com>
2021-01-21 12:10:31 -0700

    Missing CSS

--- src/components/authentication/signup.jsx
+++ src/components/authentication/signup.jsx
@@ -2,6 +2,43 @@ import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import styled from 'styled-components';
+
+const SubmitButton = styled.input`
+  background-color: #84C0C9;
+  color: white;
+  width: 25%;
+  margin: 0 auto;
+  padding: 5px;
+  border: #84C0C9;
+  border-radius: 5px;
+`;
+
+const MainStyle = styled.div`
+  background-color: black;
+`;
+
+const InnerStyle = styled.div`
+  background-color: white;
+  border: solid black 2px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledInput = styled.input`
+  width: 200px;
+  border-radius: 5px;
+  border-color: #DCDCDC;
+`;
+
+const ColumnDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 const validatePass = (pass, rePass) => {
   if (pass !== rePass) {
@@ -17,7 +54,7 @@ const validateEmail = (email) => {
 };
 
 const validatePhone = (phone) => {
-  const phoneFormat = /[0-9]{3}-[0-9]{3}-[0-9]{4}/;
+  const phoneFormat = /[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
   if (!phoneFormat.test(phone)) {
     window.alert('Please enter a valid phone number');
   }
@@ -29,43 +66,43 @@ export default function SignUp() {
   const [email, setEmail] = useState('');
 
   return (
-    <div className="m-3">
-      <h1>Create an account</h1>
-      <Container>
-        <form>
-          <Row>
-            <Col sm>
-              <p>Name</p>
-              <input type="text" placeholder="Sally Smith" />
-            </Col>
-          </Row>
-          <Row>
-            <Col sm>
-              <p>Email</p>
-              <input type="text" placeholder="sallysmith@gmail.com" onBlur={(e) => validateEmail(e.target.value)} onChange={(x) => setEmail(email.replace(email, x.target.value))} />
-            </Col>
-            <Col sm>
-              <p>Phone Number</p>
-              <input type="text" placeholder="123-456-7890" onBlur={(e) => validatePhone(e.target.value)} onChange={(x) => setPhone(phone.replace(phone, x.target.value))} />
-            </Col>
-          </Row>
-          <Row>
-            <Col sm>
-              <p>Password</p>
-              <input type="password" placeholder="******" onChange={(e) => setPassword(password.replace(password, e.target.value))} />
-            </Col>
-            <Col sm>
-              <p>Re-enter Password</p>
-              <input type="password" placeholder="******" onBlur={(e) => validatePass(password, e.target.value)} />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <input type="submit" value="Create Account" />
-            </Col>
-          </Row>
-        </form>
-      </Container>
-    </div>
+    <MainStyle className="m-3">
+      <InnerStyle>
+        <h1>Create an account</h1>
+        <Container>
+          <form>
+            <Row>
+              <Col sm>
+                <ColumnDiv>
+                  <p>Name</p>
+                  <StyledInput type="text" placeholder="Sally Smith" required />
+                </ColumnDiv>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm>
+                <p>Email</p>
+                <StyledInput type="text" placeholder="sallysmith@gmail.com" onBlur={() => validateEmail(email)} onChange={(x) => setEmail(email.replace(email, x.target.value))} required />
+              </Col>
+              <Col sm>
+                <p>Phone Number</p>
+                <StyledInput type="text" placeholder="123-456-7890" onBlur={() => validatePhone(phone)} onChange={(x) => setPhone(phone.replace(phone, x.target.value))} required />
+              </Col>
+            </Row>
+            <Row>
+              <Col sm>
+                <p>Password</p>
+                <StyledInput type="password" placeholder="******" onChange={(e) => setPassword(password.replace(password, e.target.value))} required />
+              </Col>
+              <Col sm>
+                <p>Re-enter Password</p>
+                <StyledInput type="password" placeholder="******" onBlur={(e) => validatePass(password, e.target.value)} required />
+              </Col>
+            </Row>
+            <SubmitButton type="submit" value="Create Account" />
+          </form>
+        </Container>
+      </InnerStyle>
+    </MainStyle>
   );
 }
