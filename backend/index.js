9edb52f946cf226b3a89c9c6de5d13f306089e42
Cole <ctperry0301@gmail.com>
2021-01-20 17:14:05 -0800

    added mongoose connectivity

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://dbUserCole:BP1fgVHMqPtodWI2@cluster0.4uwkp.mongodb.net/hospice-of-slo?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(() => console.log('Connected to MongoDB'))
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
