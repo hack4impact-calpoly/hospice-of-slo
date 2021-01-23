e9c9207cb60235b23590819b358320007b85122e
Vance Winstead <vance.winstead@gmail.com>
2021-01-22 18:00:11 -0800

    Used React-Icons for Navbar

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -4,13 +4,9 @@ import PropTypes from 'prop-types';
 import './navbar.css';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import CalendarBlank from './icons/CalendarBlank.svg';
-import ChatsCircle from './icons/ChatsCircle.svg';
-import ClipboardText from './icons/ClipboardText.svg';
-import Clock from './icons/Clock.svg';
-import Phone from './icons/Phone.svg';
-import SignOut from './icons/SignOut.svg';
-import User from './icons/User.svg';
+import {
+  BiUser, BiCalendarAlt, BiChat, BiClipboard, BiPhone, BiTime, BiLogOut,
+} from 'react-icons/bi';
 
 /* Styled Components */
 const SpacedDiv = styled.div`
@@ -24,11 +20,11 @@ const LinkLabel = styled.span`
 `;
 
 function NavLink(props) {
-  const { to, icon, children } = props;
+  const { to, Icon, children } = props;
   return (
     <Link to={to}>
       <SpacedDiv>
-        <img src={icon} alt="" />
+        <Icon size={32} />
         <LinkLabel>{children}</LinkLabel>
       </SpacedDiv>
     </Link>
@@ -37,20 +33,20 @@ function NavLink(props) {
 
 NavLink.propTypes = {
   to: PropTypes.string.isRequired,
-  icon: PropTypes.element.isRequired,
+  Icon: PropTypes.element.isRequired,
   children: PropTypes.string.isRequired,
 };
 
 export default function Navbar() {
   return (
     <Menu>
-      <NavLink to="/" icon={User}>Name</NavLink>
-      <NavLink to="/schedule" icon={CalendarBlank}>Schedule</NavLink>
-      <NavLink to="/discussion" icon={ChatsCircle}>Discussion</NavLink>
-      <NavLink to="/past-shifts" icon={ClipboardText}>Past Shifts</NavLink>
-      <NavLink to="/contacts" icon={Phone}>Contacts</NavLink>
-      <NavLink to="/history" icon={Clock}>History</NavLink>
-      <NavLink to="/" icon={SignOut}>SignOut</NavLink>
+      <NavLink to="/" Icon={BiUser}>Name</NavLink>
+      <NavLink to="/schedule" Icon={BiCalendarAlt}>Schedule</NavLink>
+      <NavLink to="/discussion" Icon={BiChat}>Discussion</NavLink>
+      <NavLink to="/past-shifts" Icon={BiClipboard}>Past Shifts</NavLink>
+      <NavLink to="/contacts" Icon={BiPhone}>Contacts</NavLink>
+      <NavLink to="/history" Icon={BiTime}>History</NavLink>
+      <NavLink to="/" Icon={BiLogOut}>SignOut</NavLink>
     </Menu>
   );
 }
