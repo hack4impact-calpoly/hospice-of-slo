cb52cca274c4aaab0342afd02d88621948652a3c
skylerdee3 <73thieny@gmail.com>
2021-02-14 11:28:08 -0800

    edited message again

--- src/components/authentication/functions/welcome_template.js
+++ src/components/authentication/functions/welcome_template.js
@@ -5,9 +5,7 @@ const Welcome = data => {
      <html style="margin: 0; padding: 0;">
         <body style="margin: 0; padding: 0;">
             <br />
-            <br />
             <div>Congratulations! You account has been created, you can now log in.</div>
-            <br />
             <br />
         </body>
      
