70e3434988ff3a113cced7b792376151dc824519
Jonathan Lu <jonathanlu00@gmail.com>
2021-02-20 18:52:02 -0800

    Fix eslint errors

--- src/components/pages/shiftDetails.jsx
+++ src/components/pages/shiftDetails.jsx
@@ -1,22 +1,11 @@
-/* eslint-disable */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import { BiTrash, BiPencil } from 'react-icons/bi';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import {
-   BrowserRouter as Router,
-   Switch,
-   Route,
-   Link,
-   Redirect,
-   useHistory,
-   useLocation
- } from "react-router-dom";
 
 const StyledCard = styled(Card)`
   border: none;
@@ -51,57 +40,57 @@ const StyledDiv = styled.div`
 
 const StyledModal = styled(Modal)`
   background-color: rgba(0,0,0,0.4);
-`
+`;
 
 export default function ShiftDetails(props) {
   const {
-    id, address, date, startTime, endTime, notes, isAdmin, func
+    id, address, dates, startTime, endTime, notes, isAdmin, func,
   } = props;
   const [show, setShow] = useState(false);
 
-
   async function deleteVigilDocument() {
-     setShow(false);
-     const res = await firebase.firestore().collection('vigils').doc(id).delete();
-     console.log('Delete: ', res);
-     func();
+    setShow(false);
+    const res = await firebase.firestore().collection('vigils').doc(id).delete();
+    console.log('Delete: ', res);
+    func();
   }
 
   return (
     <StyledCard>
-        { isAdmin
-          ? (
-            <StyledDiv>
-              <BiPencil size="32" onClick={() => console.log('Pencil Icon works')} className="mb-4" />
-              <BiTrash size="32" onClick={() => setShow(true)} className="mb-4" />
-            </StyledDiv>
-          )
-          : null}
-        <Card.Title className="font-weight-bold">{address}</Card.Title>
-        <Card.Text>{id}</Card.Text>
-        <Card.Text>{date}</Card.Text>
-        <Card.Text>{`${startTime} to ${endTime}`}</Card.Text>
-        <Card.Subtitle>Notes</Card.Subtitle>
-        <Card.Text>{notes}</Card.Text>
+      { isAdmin
+        ? (
+          <StyledDiv>
+            <BiPencil size="32" onClick={() => console.log('Pencil Icon works')} className="mb-4" />
+            <BiTrash size="32" onClick={() => setShow(true)} className="mb-4" />
+          </StyledDiv>
+        )
+        : null}
+      <Card.Title className="font-weight-bold">{address}</Card.Title>
+      <Card.Text>{dates}</Card.Text>
+      <Card.Text>{`${startTime} to ${endTime}`}</Card.Text>
+      <Card.Subtitle>Notes</Card.Subtitle>
+      <Card.Text>{notes}</Card.Text>
 
-        <StyledModal show={show} centered>
-           <Modal.Body>
-           Are you sure you want to delete this shift?
-           </Modal.Body>
-           <Modal.Footer>
-            <StyledButton onClick={() => setShow(false)}>Cancel</StyledButton>
-            <StyledButton onClick={() => deleteVigilDocument()}>Ok</StyledButton>
-          </Modal.Footer>
-        </StyledModal>
+      <StyledModal show={show} centered>
+        <Modal.Body>
+          Are you sure you want to delete this shift?
+        </Modal.Body>
+        <Modal.Footer>
+          <StyledButton onClick={() => setShow(false)}>Cancel</StyledButton>
+          <StyledButton onClick={() => deleteVigilDocument()}>Ok</StyledButton>
+        </Modal.Footer>
+      </StyledModal>
     </StyledCard>
   );
 }
 
 ShiftDetails.propTypes = {
   isAdmin: PropTypes.bool.isRequired,
+  id: PropTypes.string.isRequired,
   address: PropTypes.string.isRequired,
-  date: PropTypes.array.isRequired,
+  dates: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
   startTime: PropTypes.string.isRequired,
   endTime: PropTypes.string.isRequired,
   notes: PropTypes.string.isRequired,
+  func: PropTypes.func.isRequired,
 };
