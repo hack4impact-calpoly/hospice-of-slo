69cd072d052a580bdaccae615b0e1485f2b03ff7
Jonathan Lu <jonathanlu00@gmail.com>
2021-02-20 16:17:38 -0800

    Delete functionality
    Implemented the ability to delete specific vigils with the use of firebase functions and React Hooks

--- src/components/pages/Schedule.jsx
+++ src/components/pages/Schedule.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import styled from 'styled-components';
 import { BiX } from 'react-icons/bi';
 import HeaderWithNav from '../navigation/nav-header';
@@ -12,6 +13,8 @@ import DesktopCalendar from './DesktopCalendar';
 // import ShiftDetails from './shiftDetails';
 import Calendar from './mobileCalendar';
 import ShiftSignUp from './ShiftSignUp';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const StyledButton = styled.button`
   color: white;
@@ -50,27 +53,75 @@ export default function Schedule(props) {
   const { isAdmin } = props;
   const [show, setShow] = useState(false);
   const [isDesktop, setDesktop] = useState(window.innerWidth > 768);
+  const [vigils, setVigils] = useState([]);
+  const [selectVigil, setSelectVigil] = useState({ id: "", address: "", dates: [], endTime: "", startTime: "", notes: ""});
   const updateMedia = () => {
     setDesktop(window.innerWidth > 768);
   };
+
+  const fetchData = async () => {
+   const vigilRef = firebase.firestore().collection("vigils");
+   const snapshot = await vigilRef.get();
+  //  setVigils(snapshot.docs.map(doc => doc.data()));  // Can not do this because we also need to save the doc id, instantly mapping doc.data loses doc id.
+   const items = [];
+   snapshot.forEach(doc => {
+     items.push({
+        id: doc.id,
+        data: doc.data()
+     })
+  });
+  setVigils(items);
+ }
+
+
+  useEffect(() => {
+     fetchData();
+  }, []);   // This useEffect block gets whole collection of vigil documents upon rendering of this component
 
   useEffect(() => {
     window.addEventListener("resize", updateMedia);
     return () => window.removeEventListener("resize", updateMedia);
   });
 
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  function handleClose() {
+     setShow(false);
+     fetchData();  // After delete, we now want to refetch the newly updated document collection, this function is passed down as prop to shiftDetails
+  }
 
+  function handleShow(vigil) {
+   setShow(true);
+   setSelectVigil(prevState => ({
+      ...prevState,
+         id: vigil.id,
+         address: vigil.data.address,
+         dates: vigil.data.dates,
+         startTime: vigil.data.startTime,
+         endTime: vigil.data.endTime,
+         notes: vigil.data.notes
+   }));
+  }
+ 
   return (
     <div>
       <HeaderWithNav {...{ isAdmin }}>Schedule</HeaderWithNav>
       <PaddedDiv>
-        <Button variant="primary" size="sm" onClick={handleShow}>Shift Details</Button>
-        <Modal show={show} onEscapeKeyDown={handleClose} onHide={handleClose} centered>
+         {vigils.map((vigil) => (
+            <div key={vigil.id}>
+              <Button variant="primary" size="sm" onClick={() => handleShow(vigil)}>{vigil.data.address}</Button>
+            </div>
+         ))}
+        <Modal show={show} size="lg" onEscapeKeyDown={handleClose} onHide={handleClose} centered>
           <Modal.Header closeButton>Shift Details</Modal.Header>
-          <ShiftDetails isAdmin={isAdmin} address="100 Apple Drive" date="Tuesday 02/02/2021" time="8:00 AM to 12:00 PM" notes="lorem impsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum" />
+          <ShiftDetails isAdmin={isAdmin} func={handleClose}
+            id={selectVigil.id} 
+            address={selectVigil.address} 
+            date={selectVigil.date} 
+            startTime={selectVigil.startTime} 
+            endTime={selectVigil.endTime}
+            notes={selectVigil.notes} />
           <Modal.Footer>
             <StyledButton onClick={() => window.alert('Successful sign up!')}>Sign Up</StyledButton>
           </Modal.Footer>
--- src/components/pages/shiftDetails.jsx
+++ src/components/pages/shiftDetails.jsx
@@ -1,15 +1,44 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import { BiTrash, BiPencil } from 'react-icons/bi';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   Link,
+   Redirect,
+   useHistory,
+   useLocation
+ } from "react-router-dom";
 
 const StyledCard = styled(Card)`
   border: none;
   @media only screen and (max-width: 768px) {
     background-color: #F1F1F1;
     padding: 5%;
+  }
+`;
+
+const StyledButton = styled.button`
+  color: white;
+  background-color: #84C0C9;
+  border: 2px solid #84C0C9;
+  border-radius: 5px;
+  padding: 6px 10px; 
+  font-size: 14px;
+  fontFamily: Roboto;
+  
+
+  &:hover{
+    color: white;
+    background-color: #558E97;
   }
 `;
 
@@ -20,10 +49,23 @@ const StyledDiv = styled.div`
    padding: 1.5vh .5vh;
 `;
 
+const StyledModal = styled(Modal)`
+  background-color: rgba(0,0,0,0.4);
+`
+
 export default function ShiftDetails(props) {
   const {
-    address, date, time, notes, isAdmin,
+    id, address, date, startTime, endTime, notes, isAdmin, func
   } = props;
+  const [show, setShow] = useState(false);
+
+
+  async function deleteVigilDocument() {
+     setShow(false);
+     const res = await firebase.firestore().collection('vigils').doc(id).delete();
+     console.log('Delete: ', res);
+     func();
+  }
 
   return (
     <StyledCard>
@@ -31,15 +73,26 @@ export default function ShiftDetails(props) {
           ? (
             <StyledDiv>
               <BiPencil size="32" onClick={() => console.log('Pencil Icon works')} className="mb-4" />
-              <BiTrash size="32" onClick={() => console.log('Trash Icon works')} className="mb-4" />
+              <BiTrash size="32" onClick={() => setShow(true)} className="mb-4" />
             </StyledDiv>
           )
           : null}
         <Card.Title className="font-weight-bold">{address}</Card.Title>
+        <Card.Text>{id}</Card.Text>
         <Card.Text>{date}</Card.Text>
-        <Card.Text>{time}</Card.Text>
+        <Card.Text>{`${startTime} to ${endTime}`}</Card.Text>
         <Card.Subtitle>Notes</Card.Subtitle>
         <Card.Text>{notes}</Card.Text>
+
+        <StyledModal show={show} centered>
+           <Modal.Body>
+           Are you sure you want to delete this shift?
+           </Modal.Body>
+           <Modal.Footer>
+            <StyledButton onClick={() => setShow(false)}>Cancel</StyledButton>
+            <StyledButton onClick={() => deleteVigilDocument()}>Ok</StyledButton>
+          </Modal.Footer>
+        </StyledModal>
     </StyledCard>
   );
 }
@@ -47,7 +100,8 @@ export default function ShiftDetails(props) {
 ShiftDetails.propTypes = {
   isAdmin: PropTypes.bool.isRequired,
   address: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  time: PropTypes.string.isRequired,
+  date: PropTypes.array.isRequired,
+  startTime: PropTypes.string.isRequired,
+  endTime: PropTypes.string.isRequired,
   notes: PropTypes.string.isRequired,
 };
