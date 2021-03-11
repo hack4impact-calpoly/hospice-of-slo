90340410955e8abe676c3e7b060b459a1f9c8508
Ben McMann <bbmcmann@volosys.com>
2021-03-11 14:18:23 -0700

    added discussion action to be exported

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 import vigilActions from './vigilActions';
 import usersActions from './usersActions';
 import userActions from './userActions';
+import discussionActions from './discussionActions';
 
 export default {
   vigils: vigilActions,
   users: usersActions,
   user: userActions,
+  discussions: discussionActions,
 };
