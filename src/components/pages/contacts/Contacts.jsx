a6dfe6e5c98b129ac50a1a1ecbf84142f8817db7
Vance Winstead <vance.winstead@gmail.com>
2021-03-11 16:58:21 -0800

    Export Button Finished

--- src/components/pages/contacts/Contacts.jsx
+++ src/components/pages/contacts/Contacts.jsx
@@ -3,11 +3,22 @@ import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import HeaderWithNav from '../../navigation/nav-header';
 import ContactCard from './ContactCard';
+import generateCSV from './ContactsCsv';
+import { SubmitButton } from '../../../styled-components/form-components';
 
 const ListWrapper = styled.div`
   padding: 0 15%;
   display: flex;
   flex-direction: column;
+  max-width: calc(750px + 30%);
+  margin: 0 auto;
+`;
+
+const ExportButton = styled(SubmitButton)`
+  width: 30%;
+  max-width: 125px;
+  align-self: flex-end;
+  margin-bottom: 10px;
 `;
 
 export default function Contacts() {
@@ -19,6 +30,7 @@ export default function Contacts() {
     <div>
       <HeaderWithNav>Contacts</HeaderWithNav>
       <ListWrapper>
+        <ExportButton onClick={() => generateCSV(users)}>Export</ExportButton>
         {users.map((user) => <ContactCard key={user.id} name={user.name} email={user.email} phone={user.phone} color="#333333" />)}
       </ListWrapper>
     </div>
