96bcfa55fe17fbb9a149861a94e53ff3c9b3cb06
n0ximus <SageM5862@gmail.com>
2021-03-28 11:20:48 -0700

    complete task and autofix:

--- src/components/pages/contacts/Contacts.jsx
+++ src/components/pages/contacts/Contacts.jsx
@@ -1,14 +1,12 @@
-/* eslint-disable */
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
+import { Form } from 'react-bootstrap';
 import HeaderWithNav from '../../navigation/nav-header';
 import ContactCard from './ContactCard';
 import generateCSV from './ContactsCsv';
 import { SubmitButton } from '../../../styled-components/form-components';
-import {
-    Row, Col, Button, Modal, Form,
-} from 'react-bootstrap';
+
 const ListWrapper = styled.div`
   padding: 0 15%;
   display: flex;
@@ -20,51 +18,53 @@ const ListWrapper = styled.div`
 const ExportButton = styled(SubmitButton)`
   width: 30%;
   max-width: 125px;
-  align-self: flex-end;
+padding-left: 10px;
+  margin: 3px auto;
   margin-bottom: 10px;
 `;
 const FormS = styled(Form.Control)`
   font-size: 18px;
   background-color: #E5E5E5;
- width: 20%;
-  display: flex;
-  flex-direction: column;
+ width:40%;
+  margin: 0 10px;
+float: left;
   max-width: calc(750px + 30%);
-  margin: 0 auto;
-  @media screen and (max-width: 768px) {
-    font-size: 16px;  
-  }
 `;
 
 export default function Contacts() {
-    // Gets User Data from redux store
-    const users = useSelector((state) => state.users.users);
-    console.log(users);
-    const [searchTerm, setSearchTerm] = useState('');
-    
-    function compare(a, b) { // sorts in alphabetical order
-        if (a.name < b.name) {
-            return -1;
-        }
-        if (a.name > b.name) {
-            return 1;
-        }
-        return 0;
+  // Gets User Data from redux store
+  const users = useSelector((state) => state.users.users);
+  console.log(users);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  function compare(a, b) { // sorts in alphabetical order
+    if (a.name < b.name) {
+      return -1;
     }
-    return (
-        <div>
-            <HeaderWithNav>Contacts</HeaderWithNav>
-                <FormS
-                    className="mb-3" placeholder={"Enter name here"} onChange={(e) => {
-                    setSearchTerm(e.target.value);
-                    console.log(searchTerm);
-                }} placeholder='Search for a name!' />
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  }
+  return (
+    <div>
+      <HeaderWithNav>Contacts</HeaderWithNav>
 
-            <ListWrapper>
-                <ExportButton onClick={() => generateCSV(users)}>Export</ExportButton>
+      <ListWrapper>
+        <div className="search">
+          <FormS
+            className="mb-3"
+            placeholder="search..."
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
+          <ExportButton onClick={() => generateCSV(users)}>Export</ExportButton>
 
-                {users.filter(user => user.name.toLowerCase().includes(searchTerm.toLowerCase())).sort(compare).map((user) => <ContactCard key={user.id} name={user.name} email={user.email} phone={user.phone} color="#333333" />)}
-            </ListWrapper>
         </div>
-    );
+
+        {users.filter((user) => user.name.toLowerCase().includes(searchTerm.toLowerCase())).sort(compare).map((user) => <ContactCard key={user.id} name={user.name} email={user.email} phone={user.phone} color="#333333" />)}
+      </ListWrapper>
+    </div>
+  );
 }
