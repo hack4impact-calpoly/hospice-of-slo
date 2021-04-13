9cc2db0cf55e4130711d760480c7ee7355b248ba
n0ximus <SageM5862@gmail.com>
2021-04-13 15:46:45 -0700

    add search bar

--- src/components/pages/discussion/Discussion.jsx
+++ src/components/pages/discussion/Discussion.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import Row from 'react-bootstrap/Row';
+/* eslint-disable */
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import { useSelector } from 'react-redux';
 import HeaderWithNav from '../../navigation/nav-header';
 import CreateThread from './createThread';
 import Forum from './Forum';
+import { Row, Col, Form } from 'react-bootstrap';
+import styled from 'styled-components';
+
+/* search bar */
+const FormS = styled(Form.Control)`
+  font-size: 18px;
+  background-color: #E5E5E5;
+  width: 57.5%;
+    position: relative;
+margin: 0 auto;
+`;
 
 export default function Discussion() {
   const isAdmin = useSelector((state) => state.user.user.isAdmin);
+    const [searchTerm, setSearchTerm] = useState('');
 
   const storeDiscussions = useSelector((state) => state.discussions.discussions);
 
@@ -22,12 +34,21 @@ export default function Discussion() {
 
     return null;
   });
-
+    function compare(a, b) { // sorts in alphabetical order
+        if (a.name < b.name) {
+            return -1;
+        }
+        if (a.name > b.name) {
+            return 1;
+        }
+        return 0;
+    }
   // sort the discussions by most recently created
   storeDiscussions.sort((a, b) => a.dateCreated - b.dateCreated);
-  storeDiscussions.reverse();
+    storeDiscussions.reverse();
 
-  const forumsNotPinned = storeDiscussions.map((d) => {
+    /* organize formums based on search and map them */
+    const forumsNotPinned = storeDiscussions.filter((discussion) => discussion.name.toLowerCase().includes(searchTerm.toLowerCase())).sort(compare).map((d) => {
     if (!d.pinned) {
       return ([
         <Row key={d.name} className="justify-content-md-center">
@@ -42,8 +63,19 @@ export default function Discussion() {
   return (
     <div>
       <HeaderWithNav>Discussions</HeaderWithNav>
-      {isAdmin ? <CreateThread /> : null}
-      <Container fluid>
+          {isAdmin ? <CreateThread /> : null}
+              <Col className="search">
+                  <FormS
+                      className="mb-3"
+                      placeholder="search..."
+                      onChange={(e) => {
+                          setSearchTerm(e.target.value);
+                      }}
+                  />
+          </Col>
+
+          <Container fluid>
+
         {forumsPinned}
         {forumsNotPinned}
       </Container>
