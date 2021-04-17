4e5a9022b2d204bef5ff1d80b33c52dcc8740238
Jonathan Lu <jonathanlu00@gmail.com>
2021-04-17 12:31:38 -0700

    Fixed ESlint errors
    Reviewed Sage's pull request and after fixing the minor ESlint errors, everything looks perfect!

--- src/components/pages/discussion/DiscussionThread.jsx
+++ src/components/pages/discussion/DiscussionThread.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import 'firebase/firestore';
@@ -42,108 +41,108 @@ const StyledCancel = styled.button`
 `;
 
 export default function DiscussionThread() {
-    const { id } = useParams();
-    const discussions = useSelector((store) => store.discussions.discussions);
-    let discussion;
-    discussions.forEach((d) => {
-        if (d.id === id) {
-            discussion = d;
-        }
-    });
-
-    const [title, setTitle] = useState('');
-    const [posts, setPosts] = useState([]);
-    // Get Messages
-    const { messages } = discussion;
-    const [message, setMessage] = useState('');
-
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => {
-        setShow(false);
-        setMessage('');
-    };
-    const handleShow = () => setShow(true);
-
-    async function getPosts() {
-        // Get Title
-        setTitle(discussion.name);
-        // Populate each user ref from a message
-        let usersData = messages.map((message) => message.userRef.get());
-        usersData = await Promise.all(usersData);
-        const users = [];
-        usersData.forEach((user) => users.push(user.data()));
-        // Combine each message with its populated user ref
-        const populatedMessages = [];
-        for (let i = 0; i < messages.length; i += 1) {
-            populatedMessages.push({ ...(messages[i]), user: users[i] });
-        }
-        setPosts(populatedMessages);
+  const { id } = useParams();
+  const discussions = useSelector((store) => store.discussions.discussions);
+  let discussion;
+  discussions.forEach((d) => {
+    if (d.id === id) {
+      discussion = d;
     }
-    const displayEmptySignal = () => {
-        console.log(messages.length);
-        /* only display empty message if there 
+  });
+
+  const [title, setTitle] = useState('');
+  const [posts, setPosts] = useState([]);
+  // Get Messages
+  const { messages } = discussion;
+  const [message, setMessage] = useState('');
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    setShow(false);
+    setMessage('');
+  };
+  const handleShow = () => setShow(true);
+
+  async function getPosts() {
+    // Get Title
+    setTitle(discussion.name);
+    // Populate each user ref from a message. Changed "message" to "m" because of ESlint
+    let usersData = messages.map((m) => m.userRef.get());
+    usersData = await Promise.all(usersData);
+    const users = [];
+    usersData.forEach((user) => users.push(user.data()));
+    // Combine each message with its populated user ref
+    const populatedMessages = [];
+    for (let i = 0; i < messages.length; i += 1) {
+      populatedMessages.push({ ...(messages[i]), user: users[i] });
+    }
+    setPosts(populatedMessages);
+  }
+  const displayEmptySignal = () => {
+    console.log(messages.length);
+    /* only display empty message if there
          * are no messages */
-        if (messages.length <= 0) {
-            return <StyledText>This forum has no messages yet</StyledText>
-        }
+    if (messages.length <= 0) {
+      return <StyledText>This forum has no messages yet</StyledText>;
     }
+    // return dummy div, ESlint needs map() to have a default return
+    return <div />;
+  };
 
-
-    const postMessage = async () => {
-        const db = firebase.firestore();
-        const currentUser = (sessionStorage.getItem('userid'));
-        const userRef = db.collection('users').doc(currentUser);
-        const messageRef = db.collection('discussions').doc(discussion.id).collection('messages');
-        const messageData = {
-            message,
-            timeSent: firebase.firestore.FieldValue.serverTimestamp(),
-            userRef,
-        };
-        messageRef.add(messageData);
-        handleClose();
+  const postMessage = async () => {
+    const db = firebase.firestore();
+    const currentUser = (sessionStorage.getItem('userid'));
+    const userRef = db.collection('users').doc(currentUser);
+    const messageRef = db.collection('discussions').doc(discussion.id).collection('messages');
+    const messageData = {
+      message,
+      timeSent: firebase.firestore.FieldValue.serverTimestamp(),
+      userRef,
     };
+    messageRef.add(messageData);
+    handleClose();
+  };
 
-    useEffect(() => {
-        getPosts();
-    }, []);
+  useEffect(() => {
+    getPosts();
+  }, []);
 
-    return (
-        <div>
-            <HeaderWithBackArrow>{title}</HeaderWithBackArrow>
-            <FloatingActionButton>+</FloatingActionButton>
-            {displayEmptySignal()};
+  return (
+    <div>
+      <HeaderWithBackArrow>{title}</HeaderWithBackArrow>
+      <FloatingActionButton>+</FloatingActionButton>
+      {displayEmptySignal()}
       <FloatingActionButton onClick={handleShow}>+</FloatingActionButton>
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>{discussion.name}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group controlId="exampleForm.ControlTextarea1">
-                            <Form.Control
-                                as="textarea"
-                                placeholder="Write a message here..."
-                                rows={6}
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                            />
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <StyledCancel variant="secondary" onClick={handleClose}>
-                        Cancel
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{discussion.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Control
+                as="textarea"
+                placeholder="Write a message here..."
+                rows={6}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <StyledCancel variant="secondary" onClick={handleClose}>
+            Cancel
           </StyledCancel>
-                    <StyledPost variant="primary" onClick={postMessage}>
-                        Post
+          <StyledPost variant="primary" onClick={postMessage}>
+            Post
           </StyledPost>
-                </Modal.Footer>
-            </Modal>
-            <PostWrapper>
-                {posts.map((post) => <DiscussionPost key={post.timeSent} author={post.user.name} timeSent={post.timeSent.toDate()} message={post.message} />)}
-            </PostWrapper>
-        </div>
-    );
+        </Modal.Footer>
+      </Modal>
+      <PostWrapper>
+        {posts.map((post) => <DiscussionPost key={post.timeSent} author={post.user.name} timeSent={post.timeSent.toDate()} message={post.message} />)}
+      </PostWrapper>
+    </div>
+  );
 }
-
