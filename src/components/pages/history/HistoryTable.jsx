95f8779a73cb3083f807eba04a6b3e9fd9362917
Jonathan Lu <jonathanlu00@gmail.com>
2021-04-23 23:43:29 -0700

    Implemented functionality for exporting history to csv
    still running into some eslint errors

--- src/components/pages/history/HistoryTable.jsx
+++ src/components/pages/history/HistoryTable.jsx
@@ -1,10 +1,22 @@
+/*eslint-disable*/
 import React from 'react';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import { Col, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import moment from 'moment';
 import HeaderWithNav from '../../navigation/nav-header';
+import { SubmitButton } from '../../../styled-components/form-components';
+import generateCSV from './HistoryCsv';
+
+const ExportButton = styled(SubmitButton)`
+  width: 100px;
+  margin: 3px auto;
+  position: relative;
+  float: right;
+  margin-bottom: 10px;
+`;
 
 const StyledRow = styled(Row)`
   display: flex;
@@ -22,9 +34,17 @@ export default function HistoryTable() {
   const { id } = useParams();
   const storeHistory = useSelector((state) => state.historyShifts.historyShifts);
 
+  const dateOptions = { month: 'numeric', day: 'numeric', year: '2-digit' };
+  const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
+
   const vigilHistory = [];
   storeHistory.forEach((shift) => {
-    if (shift.address === id) {
+    if (shift.address === id) { // Adding new field to JSON Objects to make calculating/retrieving dates & time easier
+      shift.shiftDate = `${shift.shiftStartTime.toDate().toLocaleDateString(undefined, dateOptions)}`;
+      shift.startTime = `${shift.shiftStartTime.toDate().toLocaleTimeString(undefined, timeOptions)}`;
+      shift.endTime = `${shift.shiftEndTime.toDate().toLocaleTimeString(undefined, timeOptions)}`;
+      shift.duration = `${moment(shift.shiftEndTime.toDate()).diff(moment(shift.shiftStartTime.toDate()), 'hours')}`;
+      
       vigilHistory.push(shift);
     }
   });
@@ -50,9 +70,6 @@ export default function HistoryTable() {
 
   const table = [aRow('Name', 'Vigil', 'Date', 'Time', '#C4C4C4')];
 
-  const dateOptions = { month: 'numeric', day: 'numeric', year: '2-digit' };
-  const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
-
   vigilHistory.forEach((r) => {
     const timePer = `${r.shiftStartTime.toDate().toLocaleTimeString(undefined, timeOptions)} 
         to ${r.shiftEndTime.toDate().toLocaleTimeString(undefined, timeOptions)}`;
@@ -65,6 +82,10 @@ export default function HistoryTable() {
     <div>
       <HeaderWithNav>{id}</HeaderWithNav>
       <Container>
+        <Row>
+          <Col />
+          {table.length > 1 ? <ExportButton onClick={() => generateCSV(vigilHistory)}>Export</ExportButton> : null}
+        </Row>
         {table.length > 1 ? table : 'This vigil has no shift history.'}
       </Container>
     </div>
