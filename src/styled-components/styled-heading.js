f1994ba74385c695511e9473c50ae9cf5c67750c
Vance Winstead <vance.winstead@gmail.com>
2021-05-06 16:12:34 -0700

    BUG-5: Discussions with very long titles and messages go off the page

--- src/styled-components/styled-heading.js
+++ src/styled-components/styled-heading.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const StyledHeading = styled.h2`
   text-align: center;
   padding: 32px 0 10px;
+  margin: 0 85px;
 
   @media screen and (max-width: 768px) {
     font-size: 1.3rem;  
