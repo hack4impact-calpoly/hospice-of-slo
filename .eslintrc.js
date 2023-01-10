d0b66862634f574b375cf15729553b2844ad8fad
Ben McMann <bbmcmann@volosys.com>
2023-01-10 15:41:02 -0800

    fixed easy prettier issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,3 @@
-// module.exports = {
-//     env: {
-//       browser: true,
-//       es2021: true,
-//     },
-//     extends: ["plugin:react/recommended", "airbnb", "prettier"],
-//     overrides: [
-//       {
-//         files: ["**/*.test.js", "**/*.test.jsx"],
-//         env: {
-//           jest: true,
-//         },
-//       },
-//     ],
-//     parserOptions: {
-//       ecmaFeatures: {
-//         jsx: true,
-//       },
-//       ecmaVersion: "latest",
-//       sourceType: "module",
-//     },
-//     plugins: ["react", "prettier"],
-//     rules: {
-//       "prettier/prettier": "error",
-//       // suppress errors for missing 'import React' in files
-//       "react/react-in-jsx-scope": "off",
-//       // allow jsx syntax in js files
-//       "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-//       "no-underscore-dangle": "off",
-//       "no-plusplus": "off",
-//     },
-//   };
 module.exports = {
   env: {
     browser: true,
@@ -48,7 +16,7 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "12",
+    ecmaVersion: 12,
     sourceType: "module",
   },
   plugins: ["react", "prettier"],
