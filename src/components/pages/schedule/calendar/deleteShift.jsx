936ceb112e3ea69e08c8710db9bc179383694511
Ben McMann <bbmcmann@volosys.com>
2023-06-25 11:09:55 -0600

    partial form complete, frontend design done

--- src/components/pages/schedule/calendar/deleteShift.jsx
+++ src/components/pages/schedule/calendar/deleteShift.jsx
@@ -2,11 +2,28 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import firebase from "firebase";
-import { Modal } from "react-bootstrap";
+import { Modal, Form, Col, Row } from "react-bootstrap";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import actions from "../../../../actions";
 import { selectedInfoPropType } from "../../../../dataStructures/propTypes";
+
+const SignUpButton = styled.button`
+  color: white;
+  background-color: #84c0c9;
+  border: 2px solid #84c0c9;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  padding: 6px 10px;
+  width: 100%;
+  font-size: 14px;
+  fontfamily: Roboto;
+
+  &:hover {
+    color: white;
+    background-color: #558e97;
+  }
+`;
 
 const CenteredModal = styled(Modal.Body)`
   display: flex;
@@ -45,6 +62,9 @@ export default function DeleteShift({ showMain, selectedInfo, setShowModal }) {
   const [showEdit, setShowEdit] = useState(false);
   const history = useHistory();
 
+  const [editFName, setEditFName] = useState("");
+  const [editLName, setEditLName] = useState("");
+
   async function handleDelete(id) {
     const db = firebase.firestore();
 
@@ -78,7 +98,14 @@ export default function DeleteShift({ showMain, selectedInfo, setShowModal }) {
 
   const handleEditConfirm = () => {
     console.log("pushing changes");
+    console.log(editFName);
+    console.log(editLName);
     closeAllModals();
+  };
+
+  const handleEditChange = (e, setterFunc) => {
+    e.preventDefault();
+    setterFunc(e.target.value);
   };
 
   return (
@@ -126,16 +153,143 @@ export default function DeleteShift({ showMain, selectedInfo, setShowModal }) {
         <CenteredModalAbsolute>
           <h3>Editing shift: {selectedInfo?.volName}</h3>
           <CenteredModal>
-            <p>Form here</p>
+            <Form>
+              <Col>
+                <Row>
+                  <Col>
+                    <Form.Group>
+                      <Form.Label class="form-label">New First Name</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="first name"
+                        value={editFName}
+                        onChange={(e) => handleEditChange(e, setEditFName)}
+                        required
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        {editFName === ""
+                          ? "Please provide a first name "
+                          : null}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group>
+                      <Form.Label class="form-label">New Last Name</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="lastname"
+                        value={editLName}
+                        onChange={(e) => handleEditChange(e, setEditLName)}
+                        required
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        {editLName === ""
+                          ? "Please provide a last name "
+                          : null}
+                      </Form.Control.Feedback>
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Group>
+                      <Form.Label>New Start Date</Form.Label>
+                      <Form.Control
+                        type="date"
+                        name="startDate"
+                        placeholder="yyyy/mm/dd"
+                        value="start"
+                        // onChange={(e) => handleInputChange(e, setShiftStartDate)}
+                        required
+                      />
+                      {/* <Form.Control.Feedback type="invalid">
+                        {shiftStartDate === ""
+                          ? "Please provide a starting date "
+                          : null}
+                      </Form.Control.Feedback> */}
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group>
+                      <Form.Label>New End Date</Form.Label>
+                      <Form.Control
+                        type="date"
+                        name="endDate"
+                        placeholder="yyyy/mm/dd"
+                        value="end"
+                        // onChange={(e) => handleInputChange(e, setShiftEndDate)}
+                        required
+                        // ref={endDateRef}
+                      />
+                      {/* <Form.Control.Feedback type="invalid">
+                        {shiftEndDate === ""
+                          ? "Please provide an ending date "
+                          : null}
+                        {datesInverted
+                          ? "End date cannot come before start date "
+                          : null}
+                      </Form.Control.Feedback> */}
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Group>
+                      <Form.Label>New Start Time</Form.Label>
+                      <Form.Control
+                        type="time"
+                        name="startTime"
+                        placeholder="24-hour time"
+                        value="start time"
+                        // onChange={(e) => handleInputChange(e, setShiftStartTime)}
+                        required
+                      />
+                      {/* <Form.Control.Feedback type="invalid">
+                        {shiftStartTime === ""
+                          ? "Please provide a starting time "
+                          : null}
+                      </Form.Control.Feedback> */}
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group>
+                      <Form.Label>New End Time</Form.Label>
+                      <Form.Control
+                        type="time"
+                        name="endTime"
+                        placeholder="ex. 18:00"
+                        value="end time"
+                        // onChange={(e) => handleInputChange(e, setShiftEndTime)}
+                        required
+                        // ref={endTimeRef}
+                      />
+                      {/* <Form.Control.Feedback type="invalid">
+                        {shiftEndTime === ""
+                          ? "Please provide an ending time "
+                          : null}
+                        {endsBeforeStarts
+                          ? "End time cannot not come before start time "
+                          : null}
+                      </Form.Control.Feedback> */}
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Col>
+              <SignUpButton onClick={() => handleEditConfirm()}>
+                Save Changes
+              </SignUpButton>
+              <SignUpButton onClick={() => closeAllModals()}>
+                Cancel
+              </SignUpButton>
+            </Form>
           </CenteredModal>
-          <CenteredModal>
+          {/* <CenteredModal>
             <StyledButton centered onClick={() => handleEditConfirm()}>
               Save Changes
             </StyledButton>
-            <StyledButton centered onClick={() => closeAllModals()}>
-              Cancel
-            </StyledButton>
-          </CenteredModal>
+            
+          </CenteredModal> */}
         </CenteredModalAbsolute>
       </Modal>
     </div>
